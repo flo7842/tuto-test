e938d9ae64d863ce48e3ad1486d19080b82b25f5
florian <florian@substancesactives.com>
2021-04-06 13:32:10 +0200

    Ajout de la route pour la récupération des cours

--- src/routes/findAllPokemons copy.js
+++ src/routes/findAllPokemons copy.js
@@ -1,28 +1,29 @@
-const { Cour } = require('../db/sequelize')
+const { Pokemon } = require('../db/sequelize')
 const { Op } = require('sequelize') 
 const auth = require('../auth/auth')
 
 module.exports = (app) => {
-  app.get('/api/cour', auth, (req, res) => {
+  app.get('/api/pokemons', auth, (req, res) => {
     if(req.query.name){
       const name = req.query.name
-      
+      const limit = parseInt(req.query.limit) || 5
       
       if(name.length < 2){
         const message = "Le nombre de caractère minimum requis pour la recherche est de 2 caractères"
         return res.status(400).json({ message })
       }
 
-      return Cour.findAndCountAll({ 
+      return Pokemon.findAndCountAll({ 
         where: {
            name: {
              [Op.like] : `%${name}%`
            }
           },
-          order: ['name']
+          order: ['name'],
+          limit: limit
         })
       .then(({count, rows}) => {
-        const message = `Il y a ${count} cours qui correspondent au terme de recherche ${name}.`
+        const message = `Il y a ${count} pokémons qui correspondent au terme de recherche ${name}.`
         res.json({ message, data: rows })
       })
     } else{
